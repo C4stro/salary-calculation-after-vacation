6278c710405c334e20e8965057f93538d24a4777
C4stro <matheus.hcastro@live.com>
2023-10-17 19:33:39 -0300

    feat: initialize layout of page

--- src/salary-calculation-after-vacation/app/page.tsx
+++ src/salary-calculation-after-vacation/app/page.tsx
@@ -1,15 +1,31 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Calculo de Salário pós férias&nbsp;
-        </p>
-        
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gradient-to-r from-blue-500 to-blue-700 text-white">
+      <div className="max-w-md p-6 bg-white bg-opacity-20 backdrop-blur-lg rounded-lg text-center">
+        <h1 className="text-3xl font-bold mb-4">Calculo Salário pós Férias</h1>
+        <div className="mb-4">
+          <label className="block text-lg mb-2">Salário Mensal</label>
+          <div className="relative rounded-md shadow-md">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-500">R$</span>
+            <input
+              type="number"
+              className="w-full pl-12 pr-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Digite o salário mensal"
+            />
+          </div>
+        </div>
+        <div className="mb-4">
+          <label className="block text-lg mb-2">Dias de Férias</label>
+          <input
+            type="number"
+            className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Digite o número de dias de férias"
+          />
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-md w-full">
+          Calcular Salário pós Férias
+        </button>
       </div>
     </main>
-  )
+  );
 }
